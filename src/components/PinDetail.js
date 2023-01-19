dee76306af8f94dc3acbd7591a41ea7030729992
Kevin Everrett <everrett.developer@gmail.com>
2023-01-19 08:07:59 -0500

    CreatePin

--- src/components/PinDetail.js
+++ src/components/PinDetail.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const PinDetail = () => {
   return <div>PinDetail</div>;
---
+++ src/utils/fetchUser.js
@@ -0,0 +1,5 @@
+export const fetchUser = () => {
+  localStorage.getItem('user') !== 'undefined'
+    ? JSON.parse(localStorage.getItem('user'))
+    : localStorage.clear();
+};
